31126a7692b8989597d9d6f872fbc27a1552f937
NEELSAMEL23 <neelsamel3@gmail.com>
2025-08-12 22:55:16 +0530

    code updated

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,22 @@ dotenv.config();
 const app = express();
 
 // CORS configuration (secured for Vercel frontend)
+const allowedOrigins = process.env.CLIENT_URL.split(",");
+
 app.use(
     cors({
-        origin: process.env.CLIENT_URL || "*", // Use env variable in production
+        origin: (origin, callback) => {
+            if (!origin) return callback(null, true); // allow tools like Postman
+            if (allowedOrigins.includes(origin)) return callback(null, true);
+            return callback(new Error("Not allowed by CORS"));
+        },
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true
     })
 );
+
+
 
 // Body parser
 app.use(express.json());
--- frontend/src/services/apiPaths.js
+++ frontend/src/services/apiPaths.js
@@ -1,6 +1,6 @@
 // src/config/apiPaths.js
 
-export const BASEURL = import.meta.env.VITE_API_URL || "http://localhost:8080";
+export const BASEURL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 export const API_PATHS = {
     AUTH: {
